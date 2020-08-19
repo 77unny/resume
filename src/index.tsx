9c0d09d64d82691db9a66d827bfd4a9a386e6e7f
baekCode <sosaer88@gmail.com>
2020-08-20 01:22:12 +0900

    feat : index styled + theme적용;

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from './styles/typed-components';
+import theme from './styles/theme';
+import GlobalStyle from './styles/GlobalStyle';
 import App from './Components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root'),
+);
