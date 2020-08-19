1e7d69b6f514026dc7f6983b51bdb7a87e11859a
baekCode <sosaer88@gmail.com>
2020-08-20 01:15:30 +0900

    feat : App 컴포넌트 라우트 구현

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppPresenter from './AppPresenter';
 
-const isInput = true;
+const isInput = false;
 console.log(process.env.PUBLIC_URL);
 const AppContainer = () => <AppPresenter isInput={isInput} />;
 
